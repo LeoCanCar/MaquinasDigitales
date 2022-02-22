0551de35a75d984396b9182414d3033134ed52e2
LeoCanCar <leocancar@ciencias.unam.mx>
2022-02-22 11:23:12 -0600

    Primer commit. Agregados codigos fuente de holamundo y tipos de dato

---
+++ S2_Propedeutico/TipoDeDato/TipoDeDato.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Tamaño en bytes de <char>: " << sizeof(char) <<endl;
+    cout << "Tamaño en bytes de <bool>: " << sizeof(bool) <<endl;
+    cout << "Tamaño en bytes de <int>: " << sizeof(int) <<endl;
+    cout << "Tamaño en bytes de <float>: " << sizeof(float) <<endl;
+    cout << "Tamaño en bytes de <long>: " << sizeof(long) <<endl;
+    cout << "Tamaño en bytes de <double>: " << sizeof(double) <<endl;
+
+    return 0;
+}
