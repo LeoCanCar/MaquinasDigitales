e7cda3c9191181ab326d10ee2b93bdcecbea8717
LeoCanCar <leocancar@ciencias.unam.mx>
2022-03-03 09:32:43 -0600

    Se agrega la carpeta output source y en archivo gitignore

---
+++ S2_Propedeutico/Source/Arreglo_tipo_for.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int arr1[]= {5, 8, 6, 3, 11, 1, 9, 7};
+
+int main(){
+    int i;
+    for(i=0, i<8,i++){
+         if (arr1[i]%2 == 0){
+              cout << "Este valor es par: " << arr1[1];
+         }}else{
+             cout << "Este valor es impar: " << arr1[i];
+         }
+           
+        
+
+
+}
